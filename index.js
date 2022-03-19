4f9d693cbe1a5d2ba5aa85fe5d73b7e82fd9cc6f
Habtamu Debele <habtlig2010@gmail.com>
2022-03-18 23:43:59 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Code your solutions in this file
+// // Code your solutions in this file
+
+
+//================== TEST 1 ============================
+const names = ["Guadalupe", "Ollie", "Aki"]
+// let message = "surprise"
+function writeCards(names, message){
+    let newNames = []
+    let msg = " "
+    for(let i = 0; i < names.length; i++){
+        msg = `Thank you, ${names[i]}, for the wonderful ${message} gift!`
+        newNames.push(msg)
+    }
+    return newNames
+}
+// writeCards(names, "surprise")
+console.log(writeCards(names, "surprise"))
+
+//================== TEST 2 ============================
+
+function countDown(num){
+    for(let i = 0; num >= 0 ; i++){
+        console.log(num)
+        num--   
+    }
+    return num
+}
+// countDown(4)
+console.log(countDown(10))
